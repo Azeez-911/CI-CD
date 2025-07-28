f3ad1cfb4faa4b6f6895c483a14ad77886359bd4
Azeez-911 <sk.abdulazeez777@gmail.com>
2025-07-28 22:14:07 +0530

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/CI-CD/', // ✅ your repo name with trailing slash
   plugins: [react()],
-  base: '/CI-CD/', // 👈 This is critical for GitHub Pages deployment
 });
